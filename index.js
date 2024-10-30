20f4898099586098581dc91f81470be73fedf9f0
semitha-dev <villaddarage@gmail.com>
2024-10-30 11:22:30 +0530

    s

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://root:cakebot211K@cluster1.88z2s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        // Replace `yourDatabaseName` with the actual name of your database
+        await mongoose.connect('mongodb+srv://root:cakebot211K@cluster1.88z2s.mongodb.net/yourDatabaseName?retryWrites=true&w=majority&appName=Cluster1');
+
         console.log('MongoDB connected');
-        // Try finding documents after connection to verify
+
+        // Test query to find all documents in the Message collection
         const Message = mongoose.model('Message', new mongoose.Schema({ content: String }));
         const messages = await Message.find();
         console.log('Messages:', messages); // This should output any existing messages
